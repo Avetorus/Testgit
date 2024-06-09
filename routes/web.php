d594cdbd12c88532693e1df2761cc6006bd55165
ihsan fattah alfa <88137022+Fattahq06@users.noreply.github.com>
2024-06-09 13:25:26 +0700

    pertama

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BerandaController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/beranda', [
+    BerandaController::class ,"viewBeranda",
+]);
